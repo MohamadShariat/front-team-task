f5c4311aace2fe929442d767bbd254db2bb4bd0b
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2021-10-15 13:16:31 +0330

    End of task 1

--- js/list.js
+++ js/list.js
@@ -21,54 +21,100 @@ const users = [
     address: "Mashhad Sadr",
     role: "Admin",
   },
+  {
+    username: "M.Shariat",
+    firstName: "Mohammad",
+    lastName: "Shariat",
+    nationalCode: "0944852122",
+    birthDate: "1364/12/04",
+    city: "Mashhad",
+    mobile: "09153209950",
+    address: "Mashhad Sadr",
+    role: "Admin",
+  },
+  {
+    username: "M.Shariat",
+    firstName: "Mohammad",
+    lastName: "Shariat",
+    nationalCode: "0944852122",
+    birthDate: "1364/12/04",
+    city: "Mashhad",
+    mobile: "09153209950",
+    address: "Mashhad Sadr",
+    role: "Admin",
+  },
+  {
+    username: "M.Shariat",
+    firstName: "Mohammad",
+    lastName: "Shariat",
+    nationalCode: "0944852122",
+    birthDate: "1364/12/04",
+    city: "Mashhad",
+    mobile: "09153209950",
+    address: "Mashhad Sadr",
+    role: "Admin",
+  },
+  {
+    username: "M.Shariat",
+    firstName: "Mohammad",
+    lastName: "Shariat",
+    nationalCode: "0944852122",
+    birthDate: "1364/12/04",
+    city: "Mashhad",
+    mobile: "09153209950",
+    address: "Mashhad Sadr",
+    role: "Admin",
+  },
+  {
+    username: "M.Shariat",
+    firstName: "Mohammad",
+    lastName: "Shariat",
+    nationalCode: "0944852122",
+    birthDate: "1364/12/04",
+    city: "Mashhad",
+    mobile: "09153209950",
+    address: "Mashhad Sadr",
+    role: "Admin",
+  },
 ];
 
-//* Generate table head
-
-function generateTabaleHead(table, data) {
-  const thead = table.createTHead();
-  const row = thead.insertRow();
-
-  for (let key of data) {
-    const th = document.createElement("th");
-    const text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
-  }
-}
-
-//* Generate table body
+//* Generate table body-------------------------------------------
 
 function generateTable(table, data) {
   for (let element of data) {
     const row = table.insertRow();
 
+    //* here button are inserted----------------------------------
+
     const changePassword = document.createElement("button");
     changePassword.innerHTML = "Change Password";
-    changePassword.className = "changePasswordButton";
     document.body.appendChild(changePassword);
+    changePassword.style.cursor = "pointer";
 
     const editUser = document.createElement("button");
     editUser.innerHTML = "Edit User";
-    editUser.className = "editUserButton";
     document.body.appendChild(editUser);
+    editUser.style.cursor = "pointer";
 
     const deleteUser = document.createElement("button");
     deleteUser.innerHTML = "Delete User";
-    deleteUser.className = "deleteUserButton";
     document.body.appendChild(deleteUser);
+    deleteUser.style.cursor = "pointer";
+
+    //* here the table is filled-----------------------------------
 
     for (key in element) {
       const cell = row.insertCell();
       const text = document.createTextNode(element[key]);
       cell.appendChild(text);
+      row.appendChild(changePassword);
+      row.appendChild(editUser);
+      row.appendChild(deleteUser);
+
+
     }
   }
 }
 
-
-
 const table = document.querySelector("table");
-const data = Object.keys(users[0]);
-generateTabaleHead(table, data);
 generateTable(table, users);
